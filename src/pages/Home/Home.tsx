31bef153cc2a3b2a9cfad57582b332f1ed7732ba
Cosmos Appiah <cosmosappiah029@gmail.com>
2021-06-17 01:03:28 +0000

    still working

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react";
 import { MainNav } from "../../components/Navigationbar";
 import FadeIn from "react-fade-in";
 import { NavModal } from "../../components/NavModal";
+import busStop from "../../assets/images/undraw_Bus_stop_re_h8ej(1).svg";
 
 interface HomeProps {}
 
@@ -27,6 +28,23 @@ export const Home: FC<HomeProps> = () => {
           <div className="body container">
             <div className="header-title">
               <h1>Online Bus Ticket Generator</h1>
+            </div>
+            <div className="ticket-form"></div>
+            <div className="about-section row">
+              <div className="col-12 abt-text">
+                <h1>
+                  Do you want to go to a place on campus without hustling for
+                  ticket? Relax, your bus-stop is just a click away.
+                </h1>
+                <p>
+                  Srides, an online bus-ticket generating platform, is the
+                  solution to all your commuting problems- be it long journeys
+                  or short trails.{" "}
+                </p>
+              </div>
+              <div className="col-12 abt-img">
+                <img src={busStop} alt="Bus Stop" />
+              </div>
             </div>
           </div>
         </>
